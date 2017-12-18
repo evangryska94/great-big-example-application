2e0fb4d39570d2e80c85d61f4c869cf83c5461b6
evangryska94 <eagryska@gmail.com>
2017-12-18 13:02:42 -0600

    Removes GeneratedValue from Note Id
      + Removes validate from hibernate in tests

--- src/main/java/org/exampleapps/greatbig/domain/Note.java
+++ src/main/java/org/exampleapps/greatbig/domain/Note.java
@@ -20,7 +20,6 @@ public class Note implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) 
     private String id;
 
     @Column(name = "text")
